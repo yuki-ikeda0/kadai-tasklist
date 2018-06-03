0aa74f07fe00eb6ccbb283078e2a923dacf0872c
yuki.ikeda <workingno10@yahoo.co.jp>
2018-06-03 13:25:17 +0000

    top page修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: 'tasks#index'
+  #root to: 'tasks#index'
   root to: 'toppages#index'
   
-  resources :tasks
+  get 'signup', to: 'users#new'
+  #resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
   
 end
